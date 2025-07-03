272adc456867c7b896252e4dc7d279667efe4c35
Ian Negishi <inegishi6@gmail.com>
2025-07-03 17:30:08 +0900

    complete project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,127 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def find_closest_centroids(X, centroids):
+    """
+    Computes the centroid memberships for every example
+    
+    Args:
+        X (ndarray): (m, n) Input values      
+        centroids (ndarray): (K, n) centroids
+    
+    Returns:
+        idx (array_like): (m,) closest centroids
+    
+    """
+    K = centroids.shape[0]
+
+    idx = np.zeros(X.shape[0], dtype=int)
+
+    for i in range(X.shape[0]):
+        dist = []
+        for j in range(centroids.shape[0]):
+            norm_ij = np.linalg.norm(X[i] - centroids[j])         
+            dist.append(norm_ij)
+        idx[i] = np.argmin(dist)
+        
+    
+    return idx
+
+
+def compute_centroids(X, idx, K):
+    """
+    Returns the new centroids by computing the means of the 
+    data points assigned to each centroid.
+    
+    Args:
+        X (ndarray):   (m, n) Data points
+        idx (ndarray): (m,) Array containing index of closest centroid for each 
+                       example in X. Concretely, idx[i] contains the index of 
+                       the centroid closest to example i
+        K (int):       number of centroids
+    
+    Returns:
+        centroids (ndarray): (K, n) New centroids computed
+    """
+    
+    m, n = X.shape
+    
+    centroids = np.zeros((K, n))
+    
+    for k in range(K):
+        points = X[idx == k]
+        centroids[k] = np.mean(points, axis = 0)
+        
+    
+    return centroids
+
+
+def run_kMeans(X, initial_centroids, max_iters=10):
+    """
+    Runs the K-Means algorithm on data matrix X, where each row of X
+    is a single example
+    """
+    
+    m, n = X.shape
+    K = initial_centroids.shape[0]
+    centroids = initial_centroids
+    previous_centroids = centroids    
+    idx = np.zeros(m)
+    plt.figure(figsize=(8, 6))
+
+    for i in range(max_iters):
+        
+        print("K-Means iteration %d/%d" % (i, max_iters-1))
+        
+        idx = find_closest_centroids(X, centroids)
+        
+        centroids = compute_centroids(X, idx, K)
+    plt.show() 
+    return centroids, idx
+
+def kMeans_init_centroids(X, K):
+    """
+    This function initializes K centroids that are to be 
+    used in K-Means on the dataset X
+    
+    Args:
+        X (ndarray): Data points 
+        K (int):     number of centroids/clusters
+    
+    Returns:
+        centroids (ndarray): Initialized centroids
+    """
+    
+    randidx = np.random.permutation(X.shape[0])
+
+    centroids = X[randidx[:K]]
+    
+    return centroids
+
 original_img = plt.imread("../dataset/cat.jpg")
-print(original_img.shape)
+original_img = original_img/255
+X_img = np.reshape(original_img, (original_img.shape[0] * original_img.shape[1],3))
+
+K = 16
+max_iters = 10
+initial_centroids = kMeans_init_centroids(X_img,K)
+centroids, idx = run_kMeans(X_img, initial_centroids, max_iters)
+idx = find_closest_centroids(X_img, centroids)
+
+X_recovered = centroids[idx, :] 
+
+
+X_recovered = np.reshape(X_recovered, original_img.shape) 
+fig, ax = plt.subplots(1,2, figsize=(16,16))
+plt.axis('off')
+
+ax[0].imshow(original_img)
+ax[0].set_title('Original')
+ax[0].set_axis_off()
+
+
+ax[1].imshow(X_recovered)
+ax[1].set_title('Compressed with %d colours'%K)
+ax[1].set_axis_off()
+
+plt.show()
